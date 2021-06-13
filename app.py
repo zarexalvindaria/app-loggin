c75b71079986e0bf817d406f9f6bdf470ef2030a
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-06-14 01:53:59 +0800

    added solution

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, json
+import logging
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
+
+    app.logger.info('Main request successful')
     return "Hello world!"
+
 
 @app.route("/status")
 def status():
@@ -13,16 +18,25 @@ def status():
         status=200,
         mimetype='application/json'
     )
+
+    app.logger.info('Status endpoint was reached')
     return response
+
 
 @app.route("/metrics")
 def metrics():
     response = app.response_class(
-        response=json.dumps({"status":"success", "code":0, "data": {"UserCount": 140, "UserCountActive":23}}),
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
         status=200,
         mimetype='application/json'
     )
+    app.logger.info('Metrics endpoint was reached')
+
     return response
 
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    # logging.warning('This message will get logged on to a file')
+
     app.run(host='0.0.0.0')
