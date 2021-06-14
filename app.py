99a8a8f15a860a6d60d1f800decc6b74efc931c3
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-06-14 11:37:21 +0800

    redo the file creation

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, json
+import logging
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+
+    app.logger.info('Main request successful')
+    return "Hello world!"
+
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    app.logger.info('Status endpoint was reached.')
+    return response
+
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics endpoint was reached.')
+
+    return response
+
+
+if __name__ == "__main__":
+    # create the log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+    app.run(host='0.0.0.0')
